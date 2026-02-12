f16056b2da3772b35cd5cc95f6a4b5b847a5e2ac
Spencer Driggs <spencerdriggs@yahoo.com>
2026-02-12 10:35:45 -0600

    Update map.js UT

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ fetch("Stores.csv")
   .then(response => response.text())
   .then(csvText => {
     locations = parseCSV(csvText);
-    locations = locations.filter((place) => place.State === "SC")
+    locations = locations.filter((place) => place.State === "UT")
     initMap(); //&callback=initMap
   })
   .catch(err => {
