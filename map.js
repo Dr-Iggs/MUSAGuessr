c722854d9667eb9c469baa6fd0b78167455cbdb8
Spencer Driggs <spencerdriggs@yahoo.com>
2026-02-13 12:22:53 -0600

    v1.0
    Multiple game modes, 2025 build class, map of results, tips and hints

--- map.js
+++ map.js
@@ -4,13 +4,18 @@ const goodStores = [1200,1111,7520,7537,8500,7675,6848,5659,7964,8692,7656,8870,
                     7292, 6986, 6899, 8553, 7510, 7855, 8507, 8524, 7460, 8663, 7752, 8532, 7940, 7282,6581,
                     7209,7743,7263,6865,6588,8787,8910,7600,7689,6560, 7244,7782,7853,7567,8764,6750,8605,8951,7204,6825,8780]
 
+const newStores = [2204,5525,5527,5690,5698,5722,6504,6510,6514,6536,
+                    6543,6545,6571,6576,6596,6629,6658,6659,6682,6702,
+                    6763,6813,6842,6965,7002,7023,7046,7090,7094,7096,
+                    7140,7141,7351,7356,7374,7375,7377,7428,7510,7570,
+                    7615,7624,7847,7865,7880,7881,7890,7906,7919,7921,
+                    7922,7930,7931,7960,7961,7963,7965,7969,7983,7984,
+                    7985,7986,7988,7993,7994,8590,8615,8744,8768,8810,
+                    8818,8950,8972,8974,8975,8977,8979]
 fetch("Stores.csv")
   .then(response => response.text())
   .then(csvText => {
     locations = parseCSV(csvText);
-    //locations = locations.filter((place) => goodStores.includes(Number(place.StoreNumber)));
-    locations = locations.filter((place) => place.State === "NE" || place.State === "KS" || place.State === "MI");
-    initMap();
   })
   .catch(err => {
     console.error("Failed to load locations.csv", err);
@@ -52,7 +57,7 @@ function initMap() {
 
   correctLocation = locations[Math.floor(Math.random() * locations.length)];
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40, lng: -90 },
+    center: { lat: 40, lng: -95 },
     zoom: 4,
     disableDefaultUI: true,
     zoomControl: true,
@@ -70,6 +75,7 @@ function initMap() {
       zoom: 1,
       addressControl: false,     
       showRoadLabels: false,
+      panControl: true
     }
   );
 
@@ -126,25 +132,67 @@ function selectLocation(location) {
 window.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("submitGuess").addEventListener("click", checkLocationMatch);
   
-  document.getElementById("closeResults").addEventListener("click", () => {
+  document.querySelectorAll(".close-title").forEach(button => {
+  button.addEventListener("click", () => {
     document.getElementById("popup-overlay").classList.add("hidden");
-    document.getElementById("popup").classList.add("hidden");
-
+    document.getElementById("popup-start").classList.add("hidden");
     document.getElementById("pano").classList.remove("hidden");
-    
+    document.getElementById("ui-layer").classList.remove("hidden");
+    document.getElementById("map-container").classList.remove("hidden");
   });
+});
 
   document.addEventListener("keydown", function (event) {
   if (event.key === "Enter" || event.key === "NumpadEnter") {
     checkLocationMatch();
   }
   });
+
+  document.getElementById("choose-curated").addEventListener("click", () => {
+    locations = locations.filter((place) => goodStores.includes(Number(place.StoreNumber)));
+    initMap();
+  });
+
+  document.getElementById("choose-all").addEventListener("click", () => {
+    initMap();
+  });
+
+  document.getElementById("choose-2025").addEventListener("click", () => {
+    locations = locations.filter((place) => newStores.includes(Number(place.StoreNumber)));
+    //locations = locations.filter((place) => place.State === "NE" || place.State === "KS" || place.State === "MI");
+    initMap();
+  });
+
+  document.getElementById("show-hints").addEventListener("click", () => {
+    document.getElementById("hint").textContent = `This store is in ${correctLocation.State}`
+  });
+
+  document.getElementById("show-tips").addEventListener("click", () => {
+    document.getElementById("popup-tips").classList.remove("hidden");
+    document.getElementById("show-tips").classList.add("hidden");
+    document.getElementById("close-tips").classList.remove("hidden");
+  });
+
+  document.getElementById("close-tips").addEventListener("click", () => {
+    document.getElementById("popup-tips").classList.add("hidden");
+    document.getElementById("close-tips").classList.add("hidden");
+    document.getElementById("show-tips").classList.remove("hidden");
+    document.getElementById("pano").classList.remove("hidden");
+    document.getElementById("map-container").classList.remove("hidden");
+  });
+
+  document.getElementById("close-results").addEventListener("click", () => {
+    window.location.reload();
+  });
+  
+
+  
 });
 
 
 function checkLocationMatch() {
   document.getElementById("popup-overlay").classList.remove("hidden");
-  document.getElementById("popup").classList.remove("hidden");
+  document.getElementById("popup-result").classList.remove("hidden");
   
   const correctLatLng = new google.maps.LatLng(
     correctLocation.Latitude,
@@ -155,6 +203,54 @@ function checkLocationMatch() {
     selectedLocation.Latitude,
     selectedLocation.Longitude
   );
+
+  resultMap = new google.maps.Map(document.getElementById("map-result"), {
+    disableDefaultUI: true,
+    zoomControl: true,
+    streetViewControl: false,
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    draggable: false
+  });
+  new google.maps.Marker({
+      position: selectedLatLng,
+      map: resultMap,
+      title: (selectedLocation.StoreNumber + ": " + selectedLocation.City + ", " + selectedLocation.State),
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 4,
+        fillColor: "#ff0000",
+        fillOpacity: 1,
+        strokeWeight: 1
+      }
+    });
+  new google.maps.Marker({
+      position: correctLatLng,
+      map: resultMap,
+      title: (correctLocation.StoreNumber + ": " + correctLocation.City + ", " + correctLocation.State),
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 4,
+        fillColor: "#0000ff",
+        fillOpacity: 1,
+        strokeWeight: 1
+      }
+    });
+
+  if (correctLocation.StoreNumber != selectedLocation.StoreNumber) {
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(correctLatLng);
+    bounds.extend(selectedLatLng);
+    resultMap.fitBounds(bounds);
+  }
+  else {
+    resultMap.setCenter(correctLatLng);
+    resultMap.setZoom(5);
+  }
+  
+
+  
 
   const distanceMeters =
     google.maps.geometry.spherical.computeDistanceBetween(
@@ -173,7 +269,7 @@ function checkLocationMatch() {
   else if (correctLocation.State === selectedLocation.State) {
     document.getElementById("RightWrong").textContent = `Nice one!`;
     document.getElementById("HowClose").innerHTML =
-  `You were right to pick "${correctLocation.State}", but it was Store #${correctLocation.StoreNumber} in ${correctLocation.City}.<br>
+  `You were right to pick "${correctLocation.State}", but it was Store #${correctLocation.StoreNumber} in ${correctLocation.City} (blue).<br>
    You were ${Math.round(distanceMiles)} miles away.`;
 
     console.log(
@@ -183,8 +279,8 @@ function checkLocationMatch() {
   else {
     document.getElementById("RightWrong").textContent = `Not Quite!`;
     document.getElementById("HowClose").innerHTML =
-    `The correct store was Store #${correctLocation.StoreNumber} in ${correctLocation.City}, ${correctLocation.State}.
-    <br> You were ${Math.round(distanceMiles)} miles away.`;
+    `The correct store was Store #${correctLocation.StoreNumber} in ${correctLocation.City}, ${correctLocation.State} (blue).<br>
+    You were ${Math.round(distanceMiles)} miles away.`;
 
   };
   document.getElementById("ButtonDisplay").textContent = "Refresh the page and play again!"
